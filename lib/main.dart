8aabf1faf4011fb1f0784071320b971ec692e8f0
maka y <nord2sudjp@gmail.com>
2020-11-11 17:08:12 +0900

    Column

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,26 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   build(BuildContext context) {
+    var questions = [
+      'What is your favorite color?',
+      'What is your favorite animal?'
+    ];
+
     //return MaterialApp(home: Text('Hello!'));
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text!'),
+        //body: Text('This is my default text!'),
+        body: Column(
+          children: [
+            Text('The questions!'),
+            RaisedButton(child: Text('Answer 1'), onPressed: null),
+            RaisedButton(child: Text('Answer 2'), onPressed: null),
+            RaisedButton(child: Text('Answer 3'), onPressed: null),
+          ],
+        ),
       ),
     );
   }
