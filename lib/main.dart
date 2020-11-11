465fb7faad7826befc72aa6f900e2cfb8b673a88
maka y <nord2sudjp@gmail.com>
2020-11-11 16:11:38 +0900

    Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,16 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  @override
   build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'));
+    //return MaterialApp(home: Text('Hello!'));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('This is my default text!'),
+      ),
+    );
   }
 }
