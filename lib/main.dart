637e00e88650c18d7dbec374a3b7ebcec58b79b1
maka y <nord2sudjp@gmail.com>
2020-11-12 13:51:32 +0900

    completed first version

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,14 @@ class _MyAppState extends State<MyApp> {
 
   var _questionIndex = 0;
   var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   void _answerQuestion(int score) {
     if (_questionIndex < _questions.length) {
       print('We have more questions');
@@ -73,7 +81,7 @@ class _MyAppState extends State<MyApp> {
                 answerQuestion: _answerQuestion,
                 questionIndex: _questionIndex,
                 questions: _questions)
-            : Result(_totalScore),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
-  Result(this.resultScore);
+  final Function resetHandler;
+  Result(this.resultScore, this.resetHandler);
 
   String get resultPhrase {
     var resultText = "You did it";
@@ -19,10 +20,18 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+              onPressed: resetHandler,
+              child: Text("Restar Quize!"),
+              textColor: Colors.blue)
+        ],
       ),
     );
   }
